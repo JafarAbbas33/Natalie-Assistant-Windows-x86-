0b2c5268966095bb71328b3d566e69dc5f65766b
JafarAbbas33 <68061936+JafarAbbas33@users.noreply.github.com>
2021-05-23 14:49:17 +0530

    Delete Natalie Assistant/Temp directory

--- Natalie Assistant/Temp/brightness_test.py
+++
@@ -1,7 +0,0 @@
-
-import subprocess
-
-if True:
-    test = subprocess.Popen(["/usr/lib/gnome-settings-daemon/gsd-backlight-helper",  "--get-brightness"], stdout=subprocess.PIPE)
-    output = test.communicate()[0].decode()
-    print(output)
